5e3e6aaff300a328c27c4c47486e2fe7323a6072
Pavel Semenov <otlichnosti@gmail.com>
2019-02-26 18:24:28 +0300

    SQL formatting

---
+++ whirl-app/whirl-app-server/src/main/resources/sql/postgresql/0.1/add_parameter_boolean.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE FUNCTION add_parameter_boolean (
+    p_result function_result,
+    p_code varchar,
+    p_value boolean)
+RETURNS function_result
+AS
+$$
+DECLARE
+    v_index integer;
+    v_value varchar;
+BEGIN
+    v_index := internal_next_index (p_result);
+
+    IF p_value IS NOT NULL
+    THEN
+        v_value := p_value::varchar;
+    END IF;
+
+    IF (SELECT count (*)
+          FROM skeys (p_result.parameter_index)) > 0
+    THEN
+        p_result.parameter_index := p_result.parameter_index::hstore || hstore (v_index::varchar, p_code);
+    ELSE
+        p_result.parameter_index := hstore (v_index::varchar, p_code);
+    END IF;
+
+    IF (SELECT count (*)
+          FROM skeys (p_result.parameter_value)) > 0
+    THEN
+        p_result.parameter_value :=
+        p_result.parameter_value::hstore || hstore (p_code, v_value);
+    ELSE
+        p_result.parameter_value := hstore (p_code, v_value);
+    END IF;
+
+    IF (SELECT count (*)
+          FROM skeys (p_result.parameter_type)) > 0
+    THEN
+        p_result.parameter_type := p_result.parameter_type::hstore || hstore (p_code, 'BOOLEAN');
+    ELSE
+        p_result.parameter_type := hstore (p_code, 'BOOLEAN');
+    END IF;
+
+    RETURN p_result;
+END;
+$$
+LANGUAGE plpgsql
