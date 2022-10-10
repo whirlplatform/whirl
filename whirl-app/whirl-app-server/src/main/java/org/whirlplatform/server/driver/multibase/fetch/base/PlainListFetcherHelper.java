63c5d8491332a2c285878d2f103f9e201ee3425a
VladWick <vladwick2021@gmail.com>
2022-10-10 17:06:07 +0700

    Solve combobox problem

--- whirl-app/whirl-app-server/src/main/java/org/whirlplatform/server/driver/multibase/fetch/base/PlainListFetcherHelper.java
+++ whirl-app/whirl-app-server/src/main/java/org/whirlplatform/server/driver/multibase/fetch/base/PlainListFetcherHelper.java
@@ -1,8 +1,9 @@
 package org.whirlplatform.server.driver.multibase.fetch.base;
 
 import org.apache.empire.commons.StringUtils;
+import org.apache.empire.data.DataType;
 import org.apache.empire.db.DBColumn;
-import org.apache.empire.db.expr.column.DBValueExpr;
+import org.apache.empire.db.DBColumnExpr;
 import org.whirlplatform.meta.shared.ClassLoadConfig;
 import org.whirlplatform.meta.shared.ClassMetadata;
 import org.whirlplatform.meta.shared.TreeClassLoadConfig;
@@ -15,7 +16,7 @@ import org.whirlplatform.server.utils.TypesUtil;
 
 public class PlainListFetcherHelper extends PlainTableFetcherHelper {
 
-    public DBValueExpr listColumn;
+    public DBColumnExpr listColumn;
 
     public PlainListFetcherHelper(ConnectionWrapper connectionWrapper, DataSourceDriver factory) {
         super(connectionWrapper, factory);
@@ -25,9 +26,7 @@ public class PlainListFetcherHelper extends PlainTableFetcherHelper {
     public void prepare(ClassMetadata metadata, PlainTableElement table, ClassLoadConfig config) {
         super.prepare(metadata, table, config);
 
-        // Previous example which worked with a single column
-        //this.listColumn = this.dbTable.addColumn("", DataType.CHAR, 255, false);
-        this.listColumn = this.dbDatabase.getValueExpr(config.getLabelColumn());
+        this.listColumn = dbDatabase.getValueExpr(config.getLabelColumn(), DataType.UNKNOWN).as("asd");
 
         String query = config.getQuery();
         if (!StringUtils.isEmpty(query) && !(config instanceof TreeClassLoadConfig)) {
