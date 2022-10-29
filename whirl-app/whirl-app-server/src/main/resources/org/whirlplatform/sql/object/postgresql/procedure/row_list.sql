a94b1cb48b7dd7c4c4295e55a7948cb1abd86785
VladWick <vladwick2021@gmail.com>
2022-10-29 10:21:54 +0700

    Types fixed

--- whirl-app/whirl-app-server/src/main/resources/org/whirlplatform/sql/object/postgresql/procedure/row_list.sql
+++ whirl-app/whirl-app-server/src/main/resources/org/whirlplatform/sql/object/postgresql/procedure/row_list.sql
@@ -1,10 +1,2 @@
 CREATE TYPE row_list AS (
     list_name _row_value);
-/*
-DO $$
-    BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'row_list') THEN
-            CREATE TYPE row_list AS (
-                list_name _row_value);
-        END IF;
-    END$$;*/
--- whirl-app/whirl-app-server/src/main/resources/org/whirlplatform/sql/object/postgresql/procedure/row_value.sql
+++ whirl-app/whirl-app-server/src/main/resources/org/whirlplatform/sql/object/postgresql/procedure/row_value.sql
@@ -5,16 +5,3 @@ CREATE TYPE row_value AS
     checked boolean,
     expanded boolean
 );
-
-/*DO $$
-    BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'row_list') THEN
-            CREATE TYPE row_value AS
-            (
-                id varchar(32767),
-                selected boolean,
-                checked boolean,
-                expanded boolean
-            );
-        END IF;
-    END$$;*/
