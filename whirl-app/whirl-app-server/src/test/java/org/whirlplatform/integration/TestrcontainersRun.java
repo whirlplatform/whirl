a73144f431e70088d90d0228604971afa986fd76
Anastasia Danchenko <nastiadanchenko@gmail.com>
2022-11-14 18:19:35 +0500

    Add waiting

--- whirl-app/whirl-app-server/src/test/java/org/whirlplatform/integration/TestrcontainersRun.java
+++ whirl-app/whirl-app-server/src/test/java/org/whirlplatform/integration/TestrcontainersRun.java
@@ -2,6 +2,7 @@ package org.whirlplatform.integration;
 
 import ch.qos.logback.core.net.SocketConnector;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -13,31 +14,44 @@ import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
 import java.nio.file.Paths;
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestrcontainersRun {
 
+    private final String pathNameIndex = "src/test/resources/index.html";
+    private final String pathNameWar = "C:\\Projects\\whirl_\\whirl-app\\whirl-app-server\\target\\whirl-app-server-0.3.0-SNAPSHOT.war";
     MountableFile warFile = MountableFile.forHostPath(
-            Paths.get("target/whirl-app-server-0.3.0-SNAPSHOT.war").toAbsolutePath(), 0777);;
+            Paths.get(pathNameWar)
+                    .toAbsolutePath(), 0777);
 
-            public GenericContainer<?> container = new GenericContainer<>(
-            DockerImageName.parse("tomcat") // tomcat
-    )
-            .withExposedPorts(8090, 8091);
-
+    @Rule
+    public GenericContainer<?> container = new GenericContainer<>(
+            DockerImageName.parse("tomcat:9"))
+            .withExposedPorts(8080)
+            .withCopyToContainer(warFile, "\\usr\\local\\tomcat\\webapps\\ROOT.war")
+//            .withStartupCheckStrategy(new OneShotStartupCheckStrategy().withTimeout(Duration.ofSeconds(6)))
+            .waitingFor(Wait.forLogMessage(".* Server startup .*\\s", 6))
+//            .withCommand("--deploy \\usr\\local\\tomcat\\webapps\\ROOT.war ----contextRoot /")
+                ;
     @Rule
     public BrowserWebDriverContainer<?> chrome = new BrowserWebDriverContainer<>()
             .withAccessToHost(true)
-            .withCapabilities(new ChromeOptions());
-//            .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, target);
+            .withCapabilities(new ChromeOptions())
+            .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, Paths.get("C:\\Users\\Nastia\\Documents").toFile());
 //            .withNetwork(NETWORK);
 
-    private int localPort = 8090;
+    private Integer localPort= 8090;
+    private Integer tomcatPort;
+    private String tomcatHost;
 
 
 //    @Before
@@ -56,20 +70,38 @@ public class TestrcontainersRun {
 //    }
 
 
+    @Before
+    public void setUp() {
+        tomcatHost = container.getHost();
+        tomcatPort = container.getFirstMappedPort();
+
+    }
+
+
     @Test
     public void whenNavigatedToPage_thenHeadingIsInThePage() {
-        final String rootUrl = String.format("http://host.testcontainers.internal:%d/",localPort);
+        final String rootUrl = String.format("http://%s:%d/", tomcatHost, tomcatPort);
+        System.out.println(rootUrl);
 
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         RemoteWebDriver driver = chrome.getWebDriver();
+//        driver.get(rootUrl);
+
 
         Testcontainers.exposeHostPorts(localPort);
-        driver.get("http://host.testcontainers.internal:" + localPort);
+        driver.get("http://localhost:"+localPort+"/whirl-demo.jelastic.regruhosting.ru/");
 
-        driver.get("http://whirl-demo.jelastic.regruhosting.ru/");
-        String heading = driver.findElement(By.xpath("//div[contains(text(),'Приложение')]"))
-                .getText();
 
-        assertEquals("Приложение не доступно на этом сервере.", heading);
+//        driver.get("http://whirl-demo.jelastic.regruhosting.ru/");
+//        String heading = driver.findElement(By.xpath("//div[contains(text(),'Приложение')]"))
+//                .getText();
+//        assertEquals("Приложение не доступно на этом сервере.", heading);
+
+        assertTrue(false);
     }
 }
