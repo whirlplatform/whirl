997ff1caa00198b2a8f70cc300ad78e3352e5919
VladWick <vladwick2021@gmail.com>
2022-11-15 21:19:43 +0700

    Postgre + tomcat connection

--- whirl-app/whirl-app-server/src/test/java/org/whirlplatform/integration/TestrcontainersRun.java
+++ whirl-app/whirl-app-server/src/test/java/org/whirlplatform/integration/TestrcontainersRun.java
@@ -1,5 +1,6 @@
 package org.whirlplatform.integration;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,6 +8,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
@@ -17,29 +19,35 @@ import static org.junit.Assert.assertTrue;
 
 public class TestrcontainersRun {
 
-    //private final String pathNameWar =  "target/whirl-app-server-0.3.0-SNAPSHOT/index.html";
-    //private final String pathNameWar =  "target/whirl-app-server-0.3.0-SNAPSHOT.war";
-    //private final String pathNameWar = "target/index.html";
-    private final String pathNameWar = "src/test/resources/index.html";
+    private final String pathNameWar =  "target/whirl-app-server-0.3.0-SNAPSHOT.war";
+    //private final String pathNameWar = "src/test/resources/index.html";
     MountableFile warFile = MountableFile.forHostPath(
             Paths.get(pathNameWar), 0777);
 
     Network net = Network.newNetwork();
-
     @Rule
     public GenericContainer<?> tomcat = new GenericContainer<>(
             DockerImageName.parse("tomcat:9-jdk8"))
             .withNetwork(net)
             .withNetworkAliases("tomcat")
             .withExposedPorts(8080)
-            .withCopyToContainer(warFile, "/usr/local/tomcat/webapps/ROOT/index.html")
+            .withCopyToContainer(warFile, "/usr/local/tomcat/webapps/ROOT.war")
 //            .withStartupCheckStrategy(new OneShotStartupCheckStrategy().withTimeout(Duration.ofSeconds(6)))
             .waitingFor(Wait.forLogMessage(".* Server startup .*\\s", 1))
 //            .withCommand("--deploy \\usr\\local\\tomcat\\webapps\\ROOT.war ----contextRoot /")
             ;
-//    public GenericContainer<?> postgres = new GenericContainer<>(
-//            DockerImageName.parse("postgres:10"))
-//            .withNetwork(net);
+
+    DockerImageName POSTGRES_TEST_IMAGE = DockerImageName.parse("postgres:10");
+    @Rule
+    public PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(POSTGRES_TEST_IMAGE)
+            .withDatabaseName("whirl")
+            .withUsername("whirl")
+            .withPassword("password")
+            .withNetwork(net)
+            .withNetworkAliases("postgres")
+            .withExposedPorts(5432)
+            //.withInitScript("init_postgres.sql")
+            ;
 
     @Rule
     public BrowserWebDriverContainer<?> chrome = new BrowserWebDriverContainer<>()
@@ -71,27 +79,26 @@ public class TestrcontainersRun {
 //    }
 
 
-//    @Before
-//    public void setUp() {
-//        tomcatHost = container.getHost();
-//        tomcatPort = container.getFirstMappedPort();
-//
-//    }
+    @Before
+    public void setUp() {
+
+    }
 
 
     @Test
     public void whenNavigatedToPage_thenHeadingIsInThePage() {
+
+        postgres.start();
+
         tomcatHost = tomcat.getHost();
         tomcatPort = tomcat.getFirstMappedPort();
         System.out.println(Paths.get(pathNameWar).toAbsolutePath());
 
-
         final String rootUrl = String.format("http://%s:%d/", tomcatHost, tomcatPort);
         System.out.println(rootUrl);
 
-
         RemoteWebDriver driver = chrome.getWebDriver();
-        driver.get("http://tomcat:8080/index.html");
+        driver.get("http://tomcat:8080");
 
         try {
             String heading = driver.getTitle();
