9dfb23a3290a0732fe798d92af75b0f4ead3198c
VladWick <vladwick2021@gmail.com>
2022-11-15 21:19:43 +0700

    Postgre + tomcat connection

--- whirl-app/whirl-app-server/src/test/java/org/whirlplatform/integration/PostgreTestRun.java
+++ whirl-app/whirl-app-server/src/test/java/org/whirlplatform/integration/PostgreTestRun.java
@@ -4,15 +4,12 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.junit.Rule;
 import org.junit.Test;
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
-import org.testcontainers.utility.MountableFile;
 
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -21,17 +18,6 @@ import static org.junit.Assert.assertEquals;
 
 
 public class PostgreTestRun {
-
-//    @ClassRule
-//    public static DockerComposeContainer environment =
-//            new DockerComposeContainer(new File("C:/1-must-have/1-workspace/5-job/whirl/docker/docker-compose.yml"));
-//                    .withExposedService("redis_1", REDIS_PORT)
-//                    .withExposedService("elasticsearch_1", ELASTICSEARCH_PORT);
-
-    //private final String pathNameWar = "src/test/resources/init_postgres.sql";
-    private final String pathNameWar = "C:/1-must-have/1-workspace/5-job/whirl/docker/conf/postgresql/context.xml";
-    MountableFile warFile = MountableFile.forHostPath(
-            Paths.get(pathNameWar), 0777);
 
     DockerImageName POSTGRES_TEST_IMAGE = DockerImageName.parse("postgres:10");
     @Rule
@@ -43,32 +29,13 @@ public class PostgreTestRun {
             .withNetworkAliases("postgres")
             .withExposedPorts(5432);
 
-            //
-            //.withFileSystemBind("C:/1-must-have/1-workspace/5-job/whirl/docker/conf/postgresql/context.xml", "/usr/local/postgres", BindMode.READ_ONLY)
-            //.withDatabaseName("whirl");
-//            .withUsername("whirl")
-//            .withNetwork(net);
-
     @Test
     public void testSimple() throws SQLException, IOException, InterruptedException {
-
-//        environment.start();
-
         postgres.start();
-        //postgres.withFileSystemBind("C:/1-must-have/1-workspace/5-job/whirl/docker/conf/postgresql/context.xml", "/usr/local/postgres", BindMode.READ_ONLY);
-        postgres.withFileSystemBind("C:/1-must-have/1-workspace/5-job/whirl/docker/docker-compose.yml", "/usr/local/postgres", BindMode.READ_ONLY);
         ResultSet resultSet = performQuery(postgres, "SELECT 1");
         int resultSetInt = resultSet.getInt(1);
-        //assertThat(resultSetInt, ).as("A basic SELECT query succeeds").isEqualTo(1);
         assertEquals(resultSetInt, 1);
-        //assertHasCorrectExposedAndLivenessCheckPorts(postgres);
     }
-
-//    private void assertHasCorrectExposedAndLivenessCheckPorts(PostgreSQLContainer<?> postgres) {
-//        assertThat(postgres.getExposedPorts()).containsExactly(PostgreSQLContainer.POSTGRESQL_PORT);
-//        assertThat(postgres.getLivenessCheckPortNumbers())
-//                .containsExactly(postgres.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT));
-//    }
 
     protected ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql) throws SQLException {
         DataSource ds = getDataSource(container);
