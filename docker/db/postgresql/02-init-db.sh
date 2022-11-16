853b485d95eb5cb6ce5527da617423df9c22917b
Pavel Semenov <pavel.semenov@domru.ru>
2022-11-16 17:23:07 +0300

    testcontainers

--- docker/db/postgresql/02-init-db.sh
+++ docker/db/postgresql/02-init-db.sh
@@ -4,4 +4,6 @@ set -e
 echo "Creating database and user 'whirl' with password 'password'"
 psql -d whirl <<EOSQL
 CREATE SCHEMA whirl AUTHORIZATION whirl;
+SET search_path TO whirl;
+CREATE EXTENSION IF NOT EXISTS hstore;
 EOSQL
