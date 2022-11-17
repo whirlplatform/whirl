1faea066dba7be27f6e12766bb215156dd24f915
VladWick <vladwick2021@gmail.com>
2022-11-17 20:51:40 +0700

    DockerCompose update

--- whirl-app/whirl-app-server/src/test/java/org/whirlplatform/integration/DockerComposeTestRun.java
+++ whirl-app/whirl-app-server/src/test/java/org/whirlplatform/integration/DockerComposeTestRun.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.testcontainers.containers.DockerComposeContainer;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -16,9 +18,41 @@ public class DockerComposeTestRun {
     static final String testComposePath = "src/test/resources/compose-test.yml";
     static final String appComposePath = "src/test/resources/docker-compose.yml";
 
+    /**/
+    private static List<File> files = new ArrayList<>();
+    public static List<File> solve() {
+        getSubFiles(files, new File("C:\\1-must-have\\1-workspace\\5-job\\whirl\\docker"));
+        for (Object file : files.toArray()) {
+            System.out.println(((File) file).getAbsolutePath());
+        }
+        return files;
+    }
+    private static void getSubFiles(List<File> source, File parent) {
+        if (!source.contains(parent)) {
+            source.add(parent);
+        }
+        File[] listFiles = parent.listFiles();
+        if(listFiles == null) {
+            return;
+        }
+        for (File file : listFiles) {
+            if (file.isDirectory()) {
+                getSubFiles(source, file);
+            } else {
+                if (!source.contains(file)) {
+                    source.add(file);
+                }
+            }
+        }
+    }
+    /**/
+
     @ClassRule
     public static DockerComposeContainer environment =
-            new DockerComposeContainer(new File(appComposePath))
+//            new DockerComposeContainer(solve())
+            new DockerComposeContainer(new File("src/test/resources/docker-compose.yml"))
+                    .withLocalCompose(true)
+
 //                    .withExposedService("postgresql_1", 5432)
 //                    .withExposedService("elasticsearch_1", ELASTICSEARCH_PORT);
 
