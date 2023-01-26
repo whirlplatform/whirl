59b6f0d6ee2731a453b618e7da5b4d68ab02495c
VladWick <vladwick2021@gmail.com>
2023-01-26 20:29:31 +0700

    Rebase onto develop - fix run all problem

--- whirl-app/whirl-app-server/src/test/java/org/whirlplatform/ServerUT.java
+++ whirl-app/whirl-app-server/src/test/java/org/whirlplatform/ServerUT.java
@@ -1,19 +1,35 @@
 //package org.whirlplatform;
 //
+//import org.junit.ClassRule;
+//import org.junit.Test;
+//import org.testcontainers.containers.PostgreSQLContainer;
+//import org.testcontainers.utility.DockerImageName;
+//import org.whirlplatform.server.evolution.LiquibaseEvolutionManager;
+//
 //public class ServerUT {
 //
 //    @ClassRule
 //    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-//        DockerImageName.parse("postgres:" + DATABASE_VERSION))
+//        DockerImageName.parse("postgres:13"))
 //        .withUsername("postgres")
-//        .withNetwork(net)
+//        //.withNetwork(net)
 //        .withNetworkAliases("postgresql")
 //        .withExposedPorts(5432)
 //        .withFileSystemBind("../../docker/db/postgresql/",
 //            "/docker-entrypoint-initdb.d/");
 //
 //    @Test
-//    public void openSideex() throws IOException, InterruptedException, URISyntaxException, ParseException {
+//    public void migrationTest() {
+//        LiquibaseEvolutionManager var = new LiquibaseEvolutionManager();
+
+//          folder with migrations
+//          asd
+//
+//
+//    }
+
+//    @Test
+//    public void rollbackTest() {
 //        LiquibaseEvolutionManager var = new LiquibaseEvolutionManager();
 //
 //
@@ -22,3 +38,5 @@
 //
 //
 //}
+
+
