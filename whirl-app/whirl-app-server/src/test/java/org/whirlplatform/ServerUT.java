d0e886d2dae087d1bb8693ab54ce0c8dd53f25cd
VladWick <vladwick2021@gmail.com>
2023-02-02 19:39:00 +0700

    Finish task

--- whirl-app/whirl-app-server/src/test/java/org/whirlplatform/ServerUT.java
+++ whirl-app/whirl-app-server/src/test/java/org/whirlplatform/ServerUT.java
@@ -1,25 +1,31 @@
 package org.whirlplatform;
 
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 import org.whirlplatform.server.config.Configuration;
-import org.whirlplatform.server.config.JndiConfiguration;
+import org.whirlplatform.server.db.ConnectException;
 import org.whirlplatform.server.db.ConnectionProvider;
-import org.whirlplatform.server.db.TomcatConnectionProvider;
+import org.whirlplatform.server.driver.multibase.fetch.postgresql.PostgreSQLConnectionWrapper;
 import org.whirlplatform.server.evolution.EvolutionException;
 import org.whirlplatform.server.evolution.EvolutionManager;
 import org.whirlplatform.server.evolution.LiquibaseEvolutionManager;
 import org.whirlplatform.server.log.Logger;
 import org.whirlplatform.server.log.LoggerFactory;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static org.junit.Assert.assertTrue;
 
 public class ServerUT {
 
     private static Logger _log = LoggerFactory.getLogger(ServerUT.class);
 
-    private static String DATABASE_VERSION = "13";
+    private static String DATABASE_VERSION = "14";
 
     @ClassRule
     public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
@@ -31,40 +37,72 @@ public class ServerUT {
         .withFileSystemBind("../../docker/db/postgresql/",
             "/docker-entrypoint-initdb.d/");
 
-    @Before
-    public void createDatabase() {
-
-    }
+    String alias = "metadata";
+    String scriptPath = "org/whirlplatform/sql/changelog.xml";
+    Properties props;
+    Connection connection;
+    ConnectionProvider connectionProvider;
+    EvolutionManager evolutionManager;
+    Configuration configuration;
 
     @Test
-    public void migrationTest() throws EvolutionException {
-        _log.info("Unit test started");
+    public void migrationTest()
+        throws EvolutionException, ConnectException, SQLException, InterruptedException, IOException {
+        _log.info("Migration test started!");
 
-        ConnectionProvider connectionProvider = new TomcatConnectionProvider();
-        Configuration configuration = new JndiConfiguration();
-        EvolutionManager evolutionManager = new LiquibaseEvolutionManager(connectionProvider, configuration);
+        _log.info(postgres.getJdbcUrl());
 
-        String alias = "metadata";
-        String scriptPath = "org/whirlplatform/sql/changelog.xml";
+        props = new Properties();
+        props.setProperty("user", "whirl");
+        props.setProperty("password", "password");
 
-        // Whirl apply test
+        connection = postgres.getJdbcDriverInstance().connect("jdbc:postgresql://localhost:" + postgres.getMappedPort(5432) + "/whirl", props);
+        _log.info(connection.toString());
+
+        connectionProvider = Mockito.mock(ConnectionProvider.class);
+        _log.info(connection.toString());
+        Mockito.when(connectionProvider.getConnection(Mockito.any())).thenReturn(new PostgreSQLConnectionWrapper(alias, connection, null));
+
+        configuration = Mockito.mock(Configuration.class);
+        Mockito.when(configuration.lookup(Mockito.any(String.class))).thenReturn(true);
+
+        evolutionManager = new LiquibaseEvolutionManager(connectionProvider, configuration);
         evolutionManager.applyMetadataEvolution(alias, scriptPath);
 
-        // Applications test
-        evolutionManager.applyApplicationEvolution(alias, scriptPath);
+        // Check amount of tables
+        String str = postgres.execInContainer("psql", "-U", "whirl", "-c", "select count(*) from pg_tables where tableowner = 'whirl'").toString();
+        String substr = str.substring(str.indexOf("-------") + 7, str.indexOf("(1 row)"));
+        String result = substr.replaceAll("\\s", "");
+        int amountOfTables = Integer.valueOf(result);
+        _log.info("Amount of tables: " + amountOfTables);
+        assertTrue("Amount of tables should be greater than 0 !", amountOfTables > 0);
+
+        _log.info("Migration test finished!");
+        rollbackTest();
     }
 
-    @Test
-    public void rollbackTest() {
-        // LiquibaseEvolutionManager var = new LiquibaseEvolutionManager();
+    public void rollbackTest()
+        throws InterruptedException, EvolutionException, ConnectException, SQLException, IOException {
+        _log.info("Rollback test started!");
 
-        // rollbackApplicationEvolution
-        // rollbackMetadataEvolution
+        props = new Properties();
+        props.setProperty("user", "whirl");
+        props.setProperty("password", "password");
 
+        connection = postgres.getJdbcDriverInstance().connect("jdbc:postgresql://localhost:" + postgres.getMappedPort(5432) + "/whirl", props);
+        Mockito.when(connectionProvider.getConnection(Mockito.any())).thenReturn(new PostgreSQLConnectionWrapper(alias, connection, null));
+        evolutionManager.rollbackMetadataEvolution(alias, scriptPath);
+
+        // Check amount of tables
+        String str = postgres.execInContainer("psql", "-U", "whirl", "-c", "select count(*) from pg_tables where tableowner = 'whirl'").toString();
+        String substr = str.substring(str.indexOf("-------") + 7, str.indexOf("(1 row)"));
+        String result = substr.replaceAll("\\s", "");
+        int amountOfTables = Integer.valueOf(result);
+        _log.info("Amount of tables: " + amountOfTables);
+        assertTrue("Amount of tables should be equals to 2 !", amountOfTables == 2);
+
+        _log.info("Rollback test finished!");
     }
-
-
-
 }
 
 
