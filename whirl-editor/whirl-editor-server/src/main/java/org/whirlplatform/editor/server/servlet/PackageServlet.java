48c3012cf0a1c4144c113463f076faaa42171229
Pavel Semenov <otlichnosti@gmail.com>
2023-03-06 09:26:54 +0000

    Реализовано скачивание приложения из редактора

--- whirl-editor/whirl-editor-server/src/main/java/org/whirlplatform/editor/server/servlet/PackageServlet.java
+++ whirl-editor/whirl-editor-server/src/main/java/org/whirlplatform/editor/server/servlet/PackageServlet.java
@@ -2,14 +2,12 @@ package org.whirlplatform.editor.server.servlet;
 
 import com.google.inject.Inject;
 import java.io.IOException;
-import java.util.zip.ZipOutputStream;
 import javax.inject.Singleton;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.whirlplatform.editor.server.packager.Packager;
 import org.whirlplatform.meta.shared.ApplicationStoreData;
-import org.whirlplatform.meta.shared.editor.ApplicationElement;
 import org.whirlplatform.server.log.Logger;
 import org.whirlplatform.server.log.LoggerFactory;
 import org.whirlplatform.server.metadata.store.MetadataStore;
@@ -43,20 +41,8 @@ public class PackageServlet extends HttpServlet {
             .getAttribute(Packager.SESSION_KEY);
 
         try {
-            ApplicationElement application = metadataStore
-                .loadApplication(data.getCode(), data.getVersion());
-            if (application == null) {
-                resp.sendError(HttpServletResponse.SC_NO_CONTENT,
-                    "Application for import have not chosen");
-                return;
-            }
-            ZipOutputStream out = new ZipOutputStream(resp.getOutputStream());
-            packager.make(application, data.getVersion(), out);
-            out.flush();
-            out.finish();
-            out.close();
-
-            req.getSession().removeAttribute(Packager.SESSION_KEY);
+            metadataStore.packageToZip(data.getCode(), data.getVersion(),
+                resp.getOutputStream());
         } catch (MetadataStoreException e) {
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             _log.error("Package creating exception", e);
