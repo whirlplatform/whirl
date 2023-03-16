acce4eb11b8a43829d212cd3fad93c3e3daa65d1
Nastia Danchenko <nastiadanchenko@gmail.com>
2023-03-16 18:50:47 +0500

    Add AbstractComboBoxBuilder

--- whirl-app/whirl-app-client/src/main/java/org/whirlplatform/component/client/grid/TableComboBoxBuilder.java
+++ whirl-app/whirl-app-client/src/main/java/org/whirlplatform/component/client/grid/TableComboBoxBuilder.java
@@ -11,7 +11,7 @@ import org.whirlplatform.meta.shared.FieldMetadata;
 import org.whirlplatform.meta.shared.data.DataValue;
 import org.whirlplatform.meta.shared.data.ListModelData;
 
-class TableComboBoxBuilder extends ComboBoxBuilder<ListModelData,ComboBox<ListModelData>> {
+class TableComboBoxBuilder extends ComboBoxBuilder {
 
     private final FieldMetadata tableField;
 
