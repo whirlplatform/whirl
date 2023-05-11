5b1b5d55738170ce10b9840da62bd8e720054b8e
Nastia Danchenko <nastiadanchenko@gmail.com>
2023-05-11 14:55:37 +0500

    create getNextIdInSequence()

--- whirl-app/whirl-app-server/src/main/java/org/whirlplatform/server/driver/multibase/fetch/base/BasePlainDataChanger.java
+++ whirl-app/whirl-app-server/src/main/java/org/whirlplatform/server/driver/multibase/fetch/base/BasePlainDataChanger.java
@@ -1,7 +1,13 @@
 package org.whirlplatform.server.driver.multibase.fetch.base;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.empire.db.DBDatabase;
+import org.apache.empire.db.DBTable;
 import org.whirlplatform.server.db.ConnectionWrapper;
 import org.whirlplatform.server.driver.multibase.fetch.DataSourceDriver;
+
 
 public class BasePlainDataChanger extends AbstractPlainDataChanger {
 
@@ -15,4 +21,36 @@ public class BasePlainDataChanger extends AbstractPlainDataChanger {
         java.util.Random rnd = new java.util.Random();
         return String.valueOf(rnd.nextLong());
     }
+
+    @Override
+    protected String getNextIdInSequence(DBDatabase db, Connection connection) {
+        String query = "SELECT sl.sequence_name "
+                + "from "
+                + "(SELECT sequence_schema, sequence_name "
+                + "FROM information_schema.sequences "
+                + "WHERE SEQUENCE_SCHEMA = ?) as sl "
+                + "inner join (SELECT  t.column_name, t.column_default "
+                + "FROM INFORMATION_SCHEMA.COLUMNS t "
+                + "WHERE t.table_schema = ? "
+                + "AND t.table_name = ? "
+                + "AND t.column_default is not null) as st  "
+                + "on st.column_default = 'nextval('''||?||'.'||sl.sequence_name||'''::regclass)'";
+
+        String schema = db.getSchema();
+        for (DBTable tab : db.getTables()) {
+            String tableName = tab.getName();
+            String[] params = {schema, schema, tableName, schema};
+            try {
+                ResultSet resultSet = db.executeQuery(query, params, false, connection);
+                while (resultSet.next()) {
+                    String seqName = resultSet.getString(1);
+                    return db.getNextSequenceValue(seqName, connection).toString();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return getNextId();
+    }
+
 }
