d71b4afe9569195c89ed6f0f9a491c7b763d6ba3
Pavel Semenov <otlichnosti@gmail.com>
2023-06-30 14:02:48 +0000

    Исправление проблемы очистки кеша метаданнх при сохранении приложения в редакторе.

--- whirl-app/whirl-app-server/src/main/java/org/whirlplatform/server/monitor/mbeans/Main.java
+++ whirl-app/whirl-app-server/src/main/java/org/whirlplatform/server/monitor/mbeans/Main.java
@@ -1,5 +1,17 @@
 package org.whirlplatform.server.monitor.mbeans;
 
+import org.whirlplatform.server.metadata.container.MetadataContainer;
+
 public class Main implements MainMBean {
 
+    private MetadataContainer metadataContainer;
+
+    public Main(MetadataContainer metadataContainer) {
+        this.metadataContainer = metadataContainer;
+    }
+
+    @Override
+    public void clearMetadataStoreCache() {
+        metadataContainer.clearCache();
+    }
 }
